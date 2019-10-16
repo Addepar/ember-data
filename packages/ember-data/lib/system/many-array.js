emberjs/data

commit d7ec6664e4faf218d4d3068308043daf3cf40634

backport  a fix for #2510

packages/ember-data/lib/system/relationships/state/relationship.js
@@ -170,8 +170,19 @@ Relationship.prototype = {
 
   flushCanonical: function() {
     this.willSync = false;
+    //a hack for not removing new records
+    //TODO remove once we have proper diffing
+    var newRecords = [];
+    for (var i=0; i<this.members.list.length; i++) {
+      if (this.members.list[i].get('isNew')) {
+        newRecords.push(this.members.list[i]);
+      }
+    }
     //TODO(Igor) make this less abysmally slow
     this.members = this.canonicalMembers.copy();
+    for (i=0; i<newRecords.length; i++) {
+      this.members.add(newRecords[i]);
+    }
   },
 
   flushCanonicalLater: function() {

packages/ember-data/lib/system/record_arrays/many_array.js
@@ -65,9 +65,16 @@ export default Ember.Object.extend(Ember.MutableArray, Ember.Evented, {
 
   flushCanonical: function() {
     //TODO make this smarter, currently its plenty stupid
+    var toSet = this.canonicalState.slice(0);
+    //a hack for not removing new records
+    //TODO remove once we have proper diffing
+    var newRecords = this.currentState.filter(function(record) {
+      return record.get('isNew');
+    });
+    toSet = toSet.concat(newRecords);
     this.arrayContentWillChange(0, this.length, this.length);
-    this.set('length', this.canonicalState.length);
-    this.currentState = this.canonicalState.slice(0);
+    this.set('length', toSet.length);
+    this.currentState = toSet;
     this.arrayContentDidChange(0, this.length, this.length);
     //TODO Figure out to notify only on additions and maybe only if unloaded
     this.relationship.notifyHasManyChanged();

packages/ember-data/lib/system/relationships/state/belongs_to.js
@@ -48,6 +48,11 @@ BelongsToRelationship.prototype.addCanonicalRecord = function(newRecord) {
 
 BelongsToRelationship.prototype._super$flushCanonical = Relationship.prototype.flushCanonical;
 BelongsToRelationship.prototype.flushCanonical = function() {
+  //temporary fix to not remove newly created records if server returned null.
+  //TODO remove once we have proper diffing
+  if (this.inverseRecord && this.inverseRecord.get('isNew') && !this.canonicalState) {
+    return;
+  }
   this.inverseRecord = this.canonicalState;
   this.record.notifyBelongsToChanged(this.key);
   this._super$flushCanonical();

packages/ember-data/lib/system/store.js
@@ -1148,7 +1148,9 @@ Store = Ember.Object.extend({
       this.updateId(record, data);
     }
 
-    this._backburner.schedule('finished', record, 'adapterDidCommit', data);
+    //We first make sure the primary data has been updated
+    //TODO try to move notification to the user to the end of the runloop
+    record.adapterDidCommit(data);
   },
 
   /**

packages/ember-data/tests/integration/adapter/rest_adapter_test.js
@@ -357,12 +357,13 @@ test("create - a record on the many side of a hasMany relationship should update
       comments: [1,2]
     }],
     comments: [{
-      id: "1",
-      name: "Dat Parley Letter",
-      post: 1
-    },{
       id: "2",
       name: "Another Comment",
+      post: 1
+    },
+{
+      id: "1",
+      name: "Dat Parley Letter",
       post: 1
     }]
     // My API is returning a comment:{} as well as a comments:[{...},...]

packages/ember-data/tests/integration/relationships/has_many_test.js
@@ -260,6 +260,47 @@ test("A hasMany updated link should not remove new children", function() {
   });
 });
 
+test("A hasMany updated link should not remove new children when the parent record has children already", function() {
+  Post.reopen({
+    comments: DS.hasMany('comment', { async: true })
+  });
+
+  Comment.reopen({
+    message: DS.belongsTo('post', { async: true })
+  });
+
+  env.adapter.findHasMany = function(store, record, link, relationship) {
+    return Ember.RSVP.resolve([{id:5, body: 'hello'}]);
+  };
+
+  env.adapter.createRecord = function(store, record, link, relationship) {
+    return Ember.RSVP.resolve({
+      links: {
+        comments: '/some/link'
+      }
+    });
+  };
+
+  run(function() {
+    var post = env.store.createRecord('post', {});
+    env.store.createRecord('comment', {message: post});
+
+    post.get('comments')
+      .then(function(comments) {
+        equal(comments.get('length'), 1);
+
+        return post.save();
+      })
+      .then(function() {
+        return post.get('comments');
+      })
+      .then(function(comments) {
+        equal(comments.get('length'), 2);
+      });
+  });
+});
+
+
 test("A hasMany relationship can be reloaded if it was fetched via a link", function() {
   Post.reopen({
     comments: DS.hasMany('comment', { async: true })

Addepar/ember-data

commit 1e85d9a4342fd72e9c3c99cc438c1388ee41b91b

[BUG] new embedded hasMany records ar not managed properly when saving
Introduced with https://github.com/emberjs/data/commit/d7ec6664e4faf218d4d3068308043daf3cf40634

packages/ember-data/lib/system/many-array.js
@@ -79,10 +79,10 @@ export default Ember.Object.extend(Ember.MutableArray, Ember.Evented, {
 
     //a hack for not removing new records
     //TODO remove once we have proper diffing
-    var newRecords = this.currentState.filter(function(internalModel) {
-      return internalModel.isNew();
-    });
-    toSet = toSet.concat(newRecords);
+    // var newRecords = this.currentState.filter(function(internalModel) {
+    //   return internalModel.isNew();
+    // });
+    // toSet = toSet.concat(newRecords);
     var oldLength = this.length;
     this.arrayContentWillChange(0, this.length, toSet.length);
     this.set('length', toSet.length);

packages/ember-data/lib/system/relationships/state/relationship.js
@@ -186,17 +186,17 @@ Relationship.prototype = {
     this.willSync = false;
     //a hack for not removing new records
     //TODO remove once we have proper diffing
-    var newRecords = [];
-    for (var i=0; i<this.members.list.length; i++) {
-      if (this.members.list[i].isNew()) {
-        newRecords.push(this.members.list[i]);
-      }
-    }
+    // var newRecords = [];
+    // for (var i=0; i<this.members.list.length; i++) {
+    //   if (this.members.list[i].isNew()) {
+    //     newRecords.push(this.members.list[i]);
+    //   }
+    // }
     //TODO(Igor) make this less abysmally slow
     this.members = this.canonicalMembers.copy();
-    for (i=0; i<newRecords.length; i++) {
-      this.members.add(newRecords[i]);
-    }
+    // for (i=0; i<newRecords.length; i++) {
+    //   this.members.add(newRecords[i]);
+    // }
   },
 
   flushCanonicalLater: function() {