062467880fff8c435e8dd1eb1d46ad30abe289a3
Martin Muñoz <martin.munoz@addepar.com>
2021-10-08 12:44:57 -0400

    Convert one-sided inverse assertion into deprecation

--- addon/-private/system/relationships/relationship-payloads-manager.js
+++ addon/-private/system/relationships/relationship-payloads-manager.js
@@ -1,6 +1,6 @@
 import { get } from '@ember/object';
 // import { DEBUG } from '@glimmer/env';
-import { assert } from '@ember/debug';
+import { deprecate } from '@ember/debug';
 import { default as RelationshipPayloads, TypeCache } from './relationship-payloads';
 
 /**
@@ -250,7 +250,14 @@ export default class RelationshipPayloadsManager {
       inverseCache.get(inverseBaseModelName, inverseRelationshipName);
     if (cached) {
       // TODO this assert can be removed if the above assert is enabled
-      assert(`The ${inverseBaseModelName}:${inverseRelationshipName} relationship declares 'inverse: null', but it was resolved as the inverse for ${baseModelName}:${relationshipName}.`, cached.hasInverse !== false);
+      if (cached.hasInverse === false) {
+        deprecate(`The ${inverseBaseModelName}:${inverseRelationshipName} relationship declares 'inverse: null', but it was resolved as the inverse for ${baseModelName}:${relationshipName}.`, false, {
+          id: 'ds-patched.relationships.one-sided-inverse',
+          until: '3.0.0'
+        });
+
+        return null;
+      }
 
       let isLHS = cached.lhs_baseModelName === baseModelName;
       let modelNames = isLHS ? cached.lhs_modelNames : cached.rhs_modelNames;
