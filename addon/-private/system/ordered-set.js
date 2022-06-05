66bf9f4357f9a1d8a070c26a583de366825c7a74
Martin Munoz <martin.munoz@addepar.com>
2022-06-05 10:25:32 -0400

    Use JavaScript subclasses

--- addon/-private/system/ordered-set.js
+++ addon/-private/system/ordered-set.js
@@ -1,37 +1,26 @@
 import { guidFor } from '@ember/object/internals';
 import EmberOrderedSet from '@ember/ordered-set';
 
-export default function OrderedSet() {
-  this._super$constructor();
+export default class OrderedSet extends EmberOrderedSet {
+  addWithIndex(obj, idx) {
+    let guid = guidFor(obj);
+    let presenceSet = this.presenceSet;
+    let list = this.list;
+
+    if (presenceSet[guid] === true) {
+      return;
+    }
+
+    presenceSet[guid] = true;
+
+    if (idx === undefined || idx === null) {
+      list.push(obj);
+    } else {
+      list.splice(idx, 0, obj);
+    }
+
+    this.size += 1;
+
+    return this;
+  }
 }
-
-OrderedSet.create = function() {
-  let Constructor = this;
-  return new Constructor();
-};
-
-OrderedSet.prototype = Object.create(EmberOrderedSet.prototype);
-OrderedSet.prototype.constructor = OrderedSet;
-OrderedSet.prototype._super$constructor = EmberOrderedSet;
-
-OrderedSet.prototype.addWithIndex = function(obj, idx) {
-  let guid = guidFor(obj);
-  let presenceSet = this.presenceSet;
-  let list = this.list;
-
-  if (presenceSet[guid] === true) {
-    return;
-  }
-
-  presenceSet[guid] = true;
-
-  if (idx === undefined || idx === null) {
-    list.push(obj);
-  } else {
-    list.splice(idx, 0, obj);
-  }
-
-  this.size += 1;
-
-  return this;
-};
